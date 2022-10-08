20c47f52cf85716584e2976d24674dfa64706152
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-10-08 21:21:32 +0200

    day 17

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -59,6 +59,15 @@ public class BrowserUtils {
         Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
     }
 
+    /**
+     * This method will accept a String as expected value and verify actual URL CONTAINS the value.
+     * @param expectedInURL
+     */
+    public static void verifyURLContains(String expectedInURL){
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
+    }
+
     /*
     Creating a utility method for ExplicitWait so we do not have to repeat the lines
      */
