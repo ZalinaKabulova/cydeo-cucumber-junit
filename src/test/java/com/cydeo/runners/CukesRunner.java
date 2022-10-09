b28b434a15caa82f3e7542ebba71b3bcd8768778
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-10-10 01:16:40 +0200

    day 18

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,11 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@femaleScientists"
 )
 public class CukesRunner {
 }
