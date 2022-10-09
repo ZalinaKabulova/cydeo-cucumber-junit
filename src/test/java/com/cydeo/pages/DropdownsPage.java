fa638a255fcd1ce89ca7e8b16cee248d1699f665
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-10-09 10:30:46 +0200

    day 17

---
+++ src/test/java/com/cydeo/pages/DropdownsPage.java
@@ -0,0 +1,14 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DropdownsPage {
+
+    public DropdownsPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+}
--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -19,4 +19,38 @@ public class WebTableLoginPage {
 
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
+
+    /**
+     * No parameters.
+     * When we call this method, it will directly login using
+     *
+     * Username: Test
+     * Password: Tester
+     */
+    public void login(){
+        this.inputUsername.sendKeys("Test");
+        this.inputPassword.sendKeys("Tester");
+        this.loginButton.click();
+    }
+
+    /**
+     * This method will accept two arguments and login.
+     * @param username
+     * @param password
+     */
+    public void login(String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+    }
+
+    /**
+     * This method will log in using credentials from
+     * configuration.properties
+     */
+    public void loginWithConfig(){
+        inputUsername.sendKeys(com.cydeo.utilities.ConfigurationReader.getProperty("web.table.username"));
+        inputPassword.sendKeys(com.cydeo.utilities.ConfigurationReader.getProperty("web.table.pw"));
+        loginButton.click();
+    }
 }
